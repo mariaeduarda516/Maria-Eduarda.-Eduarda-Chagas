c53abc9c51ec0114cdb7d4a6b879bcb95fa64096
mariaeduarda516 <108932069+mariaeduarda516@users.noreply.github.com>
2024-03-15 09:49:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,13 +8,40 @@ for(let i=0;i <botoes.clientHeight;i++) {
         }
         botoes[i].classlist.add("ativo");
     }
+function calculatempo(tempoobjetivo){
+    let tempoatual= new date ();
+    let tempofinal= tempoobjetivo=tempoatual;
+    let segundos= Math.floor(tempofinal/1000);
+    let minutos= Math.floor(segundos/60);
+    let horas= Math.floor(minutos/60);
+    let dias= Math.floor(horas/24);
 
-
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+    if(tempofinal>0){
+    return dias + "dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+}else{
+    return"prazo inspirado"
 }
+}
+function atualizacronometro(){
+    for(let i=0; i<contadores.length,i++){
+        contadores[i].textcontent=calculatempo(tempos[i]);
+    }
+}
+function comecacronometro(){
+    atualizacronometro();
+    setinterval(atualizacronometro,1000);
+}
+comecacronometro();
 const contadores = document.querySelectorAll(".contador");
 const tempoobjetivo1 = new date("2024-10-05T00:00:00");
-let tempoatual = new date()
 contadores [0].textcontent = tempoobjetivo1-tempoatual
-    
+const tempoobjetivo2 = new date ("2024-05-01t00:00:00");
+const tempoobjetivo3 = new date ("2024-08-01t00:00:00");
+const tempoobjetivo4 = new date ("2024-12-01t00:00:00");
+
+const tempos = [tempoobjetivo1, tempoobjetivo2, tempoobjetivo3]
 
     
