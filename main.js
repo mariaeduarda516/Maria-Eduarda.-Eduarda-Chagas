174d9800771ab060c4c20e178ab672228da7ca11
mariaeduarda516 <108932069+mariaeduarda516@users.noreply.github.com>
2024-03-22 09:01:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -42,6 +42,36 @@ const tempoobjetivo2 = new date ("2024-05-01t00:00:00");
 const tempoobjetivo3 = new date ("2024-08-01t00:00:00");
 const tempoobjetivo4 = new date ("2024-12-01t00:00:00");
 
-const tempos = [tempoobjetivo1, tempoobjetivo2, tempoobjetivo3]
+const tempos = [tempoobjetivo1, tempoobjetivo2, tempoobjetivo3,tempoobjetivo4];
 
-    
+
+function calculatempo(tempoobjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %=60;
+    horas %= 24;
+    if (tempoFinal > 0){
+        return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    } else {
+        return "Prazo Finalizado";
+    }
+}
+   
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizacronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
